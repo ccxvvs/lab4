452988503a92a5db7b28a5cb1ea7223cf88de3c3
Nishant Parikh <parikhnn@umich.edu>
2026-02-07 19:04:39 -0500

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,17 +1,16 @@
 import pandas as pd
 from datasets import load_dataset
 
-# 1. Define the Hugging Face dataset ID
+# 1. Hugging Face dataset ID
 dataset_id = "IrwinLab/LSD_mPro_Fink_2023_noncovalent_Screen2_ZINC22"
 
 print(f"Streaming dataset: {dataset_id}...")
 
-# 2. Stream the dataset (don't download the whole thing)
-# We stream to handle the massive size efficiently
+# Stream the dataset
 ds = load_dataset(dataset_id, split="train", streaming=True)
 
 # 3. Shuffle and take 50,000 samples
-# buffer_size ensures we get a good random mix from the stream
+
 shuffled_ds = ds.shuffle(seed=42, buffer_size=10000)
 data_subset = list(shuffled_ds.take(50000))
 
